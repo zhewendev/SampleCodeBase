35f8f4efac4e563274f626b96701f20f296f628c
zhewen <zhewen.dev@gmail.com>
2020-04-01 00:48:40 +0800

    [D200401]补充FragmentStudy工程代码

--- BasicModule/FragmentStudy/app/src/main/java/com/baiheng/fragmentstudy/MainActivity.java
+++ BasicModule/FragmentStudy/app/src/main/java/com/baiheng/fragmentstudy/MainActivity.java
@@ -1,37 +1,121 @@
 package com.baiheng.fragmentstudy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
+import android.widget.Toast;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+import java.lang.ref.WeakReference;
 
 /**
  * Fragment的主活动页面
  *
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RightFragment.ActivityListener{
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    public MyHandler mMyHandler = new MyHandler(this);
     private FragmentManager mFragmentManager;
     private FragmentTransaction mFragmentTransaction;
+
+    public static class MyHandler extends Handler {
+        private WeakReference<MainActivity> mActivity;
+
+        public MyHandler(MainActivity activity) {
+            mActivity = new WeakReference<MainActivity>(activity);
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 1:
+                    Log.d(TAG,"正在处理RightFragment的Handler消息");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(TAG,"onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dynamicLoadFragment(LeftFragment.newInstance("Hello, I'm MainActivity"));
+        EventBus.getDefault().register(this);   //注册EventBus
         dynamicLoadFragment(new RightFragment());
+        registerBroadcastReceiver();
     }
 
+    /**
+     * 动态加载Fragment
+     * @param fragment
+     */
     private void dynamicLoadFragment(Fragment fragment) {
         mFragmentManager = getSupportFragmentManager();
         mFragmentTransaction = mFragmentManager.beginTransaction();
         mFragmentTransaction.add(R.id.fragment_contain,fragment);
         mFragmentTransaction.addToBackStack(null);
         mFragmentTransaction.commit();
+    }
+
+    @Override
+    public void listener(String message) {
+        Toast.makeText(this,"interface callback" + message,Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * 接收消息
+     * @param fragmentEvent
+     */
+    @Subscribe
+    public void onEventMainThred(FragmentEvent fragmentEvent) {
+        Log.d(TAG,"userName = " + fragmentEvent.getUserName() + "userAge = " + fragmentEvent.getUserAge());
+        Toast.makeText(this,"userName = " + fragmentEvent.getUserName() + "userAge = " + fragmentEvent.getUserAge(),Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * 注册广播
+     */
+    private void registerBroadcastReceiver() {
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("action_name");
+        registerReceiver(mBroadcastReceiver,intentFilter);
+    }
+
+    /**
+     * 创建广播接收器
+     */
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals("action_name")) {
+                Toast.makeText(MainActivity.this,"广播",Toast.LENGTH_LONG).show();
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
+        unregisterReceiver(mBroadcastReceiver);
+        EventBus.getDefault().unregister(this);
     }
 }
--- BasicModule/FragmentStudy/app/src/main/java/com/baiheng/fragmentstudy/RightFragment.java
+++ BasicModule/FragmentStudy/app/src/main/java/com/baiheng/fragmentstudy/RightFragment.java
@@ -1,22 +1,96 @@
 package com.baiheng.fragmentstudy;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class RightFragment extends Fragment {
+import org.greenrobot.eventbus.EventBus;
+
+public class RightFragment extends Fragment implements View.OnClickListener {
     private static final String TAG = RightFragment.class.getSimpleName();
+
+    private Handler mHandler;
+    private Button mHandlerBtn;
+    private Button mBroadcastBtn;
+    private Button mEventBusBtn;
+    private Button mInterfaceCallbackBtn;
+    private Activity mActivity;
+    private ActivityListener mActivityListener;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        Log.d(TAG,"onAttach");
+        super.onAttach(context);
+        mActivity = (Activity) context;
+        mActivityListener = (ActivityListener) context;
+        if (context instanceof MainActivity) {
+            mHandler = ((MainActivity)context).mMyHandler;
+        }
+
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         Log.d(TAG,"onCreateView");
-        return inflater.inflate(R.layout.fragment_right,container,false);
+        View root = inflater.inflate(R.layout.fragment_right,container,false);
+        init(root);
+        return root;
+    }
+
+    private void init(View view) {
+        mHandlerBtn = (Button) view.findViewById(R.id.btn_handler_connect);
+        mBroadcastBtn = (Button) view.findViewById(R.id.btn_broadcast_connect);
+        mEventBusBtn = (Button) view.findViewById(R.id.btn_event_bus_connect);
+        mInterfaceCallbackBtn = (Button) view.findViewById(R.id.btn_interface_callback);
+        mHandlerBtn.setOnClickListener(this);
+        mBroadcastBtn.setOnClickListener(this);
+        mEventBusBtn.setOnClickListener(this);
+        mInterfaceCallbackBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.btn_handler_connect:
+                if (mHandler != null) {
+                    Message message = Message.obtain();
+                    message.what = 1;
+                    mHandler.sendMessage(message);
+                }
+                break;
+            case R.id.btn_broadcast_connect:
+                Intent intent = new Intent("action_name");
+                mActivity.sendBroadcast(intent);
+                break;
+            case R.id.btn_event_bus_connect:
+                FragmentEvent fragmentEvent = new FragmentEvent();
+                fragmentEvent.setUserAge(20);
+                fragmentEvent.setUserName("龙天越");
+                EventBus.getDefault().post(fragmentEvent);
+                break;
+            case R.id.btn_interface_callback:
+                mActivityListener.listener("RightFragment Callback");
+                break;
+            default:
+                break;
+        }
+    }
+
+    public interface ActivityListener{
+        void listener(String message);
     }
 }
