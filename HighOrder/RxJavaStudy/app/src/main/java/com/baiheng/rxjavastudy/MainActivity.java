e19f8e0c588ecf4ef74239204f8ca222503a172d
ruanwen <11085273@bbktel.com>
2020-04-28 22:00:44 +0800

    [D200428]补充RxJavaStudy工程

---
+++ HighOrder/RxJavaStudy/app/src/main/java/com/baiheng/rxjavastudy/MainActivity.java
@@ -0,0 +1,61 @@
+package com.baiheng.rxjavastudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableEmitter;
+import io.reactivex.rxjava3.core.ObservableOnSubscribe;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                Log.d(TAG, "=========================currentThread name: " + Thread.currentThread().getName());
+                e.onNext(1);
+                e.onNext(2);
+                e.onNext(3);
+                e.onComplete();
+            }
+        });
+
+
+
+        Observer<Integer> observer = new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "======================onSubscribe");
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.d(TAG, "======================onNext " + integer);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "======================onError");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "======================onComplete");
+            }
+        };
+
+        observable.subscribe(observer);
+
+
+    }
+
+}
