bfebdda5d669e406d5d021011dd3e70b11f1f69c
ruanwen <11085273@bbktel.com>
2020-04-29 18:57:59 +0800

    [D200429]补充RxJava工程

--- HighOrder/RxJavaStudy/app/src/main/java/com/baiheng/rxjavastudy/MainActivity.java
+++ HighOrder/RxJavaStudy/app/src/main/java/com/baiheng/rxjavastudy/MainActivity.java
@@ -1,59 +1,70 @@
 package com.baiheng.rxjavastudy;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.ObservableEmitter;
 import io.reactivex.rxjava3.core.ObservableOnSubscribe;
+import io.reactivex.rxjava3.core.ObservableSource;
 import io.reactivex.rxjava3.core.Observer;
 import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Consumer;
+import io.reactivex.rxjava3.functions.Function;
+import io.reactivex.rxjava3.functions.Supplier;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    Integer i = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
+//
+        Observable.create(new ObservableOnSubscribe<Integer>() {
             @Override
-            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
-                Log.d(TAG, "=========================currentThread name: " + Thread.currentThread().getName());
+            public void subscribe(@NonNull ObservableEmitter<Integer> e) throws Exception {
                 e.onNext(1);
                 e.onNext(2);
                 e.onNext(3);
-                e.onComplete();
             }
-        });
-
-
-
-        Observer<Integer> observer = new Observer<Integer>() {
+        }).flatMap(new Function<Integer, ObservableSource<String>>() {
             @Override
-            public void onSubscribe(Disposable d) {
-                Log.d(TAG, "======================onSubscribe");
+            public ObservableSource<String> apply(@NonNull Integer integer) throws Exception {
+                List<String> list = new ArrayList<>();
+                for (int i = 0; i < 3; i++) {
+                    list.add("I am value " + integer);
+                }
+                int delayTime = (int) (1 + Math.random() * 10);
+                return Observable.fromIterable(list).delay(delayTime, TimeUnit.MILLISECONDS);
             }
+        }).subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(@NonNull String s) throws Exception {
+                        Log.e(TAG, "flatMap : accept : " + s + "\n");
+//                        mRxOperatorsText.append("flatMap : accept : " + s + "\n");
+                    }
+                });
 
-            @Override
-            public void onNext(Integer integer) {
-                Log.d(TAG, "======================onNext " + integer);
-            }
 
-            @Override
-            public void onError(Throwable e) {
-                Log.d(TAG, "======================onError");
-            }
 
-            @Override
-            public void onComplete() {
-                Log.d(TAG, "======================onComplete");
-            }
-        };
 
-        observable.subscribe(observer);
+
 
 
     }
