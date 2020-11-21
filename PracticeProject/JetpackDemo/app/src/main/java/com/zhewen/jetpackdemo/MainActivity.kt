1497dcc7ee817c3c0d14269ad6dae6ee79ac0b75
zhewendev <zhewen.dev@gmail.com>
2020-11-21 12:03:28 +0800

    [D201121]补充liveData示例

--- PracticeProject/JetpackDemo/app/src/main/java/com/zhewen/jetpackdemo/MainActivity.kt
+++ PracticeProject/JetpackDemo/app/src/main/java/com/zhewen/jetpackdemo/MainActivity.kt
@@ -1,16 +1,36 @@
 package com.zhewen.jetpackdemo
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.Lifecycle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.arch.core.util.Function
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import com.zhewen.jetpackdemo.lifecycle.MyObserver
 
+
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         lifecycle.addObserver(MyObserver())
+        val mutableLiveData = MutableLiveData<String>()
+        mutableLiveData.observe(this) {
+            Log.d(TAG, "onChanged:$it")
+            Toast.makeText(this, it, Toast.LENGTH_LONG).show()
+        }
+        val transformedLiveData = Transformations.map(mutableLiveData) {name ->
+            "${name}LiveData is great"
+        }
+        transformedLiveData.observe(this){
+            Log.d(TAG,"onChange2$it")
+        }
+        mutableLiveData.postValue("Hello LiveData ")
     }
 }
